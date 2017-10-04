34a16aadc76e35ff92d450809dc320bf555c6fe3
mfalcier <marcofalcier@gmail.com>
2017-10-05 01:06:14 +0200

    :sparkles: Added Circuit Breaker

--- src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -1,8 +1,10 @@
 package com.example.reservationclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
@@ -12,18 +14,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @EnableFeignClients
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ReservationClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationClientApplication.class, args);
+    }
 }
 
 class Reservation {
@@ -40,6 +44,7 @@ interface ReservationReader {
     Resources<Reservation> read();
 }
 
+
 @RestController
 @RequestMapping("/reservations")
 class ReservationApiGateway {
@@ -51,6 +56,11 @@ class ReservationApiGateway {
         this.reservationReader = reservationReader;
     }
 
+    public Collection<String> fallback() {
+        return new ArrayList<>();
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback") // circuit breaker
     @GetMapping("/names")
     public Collection<String> names() {
         return this.reservationReader
